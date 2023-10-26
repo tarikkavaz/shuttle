edb0ba681b2a70222f1445bf4033fa8bf679f0fb
Tarik Kavaz <tarikkavaz@gmail.com>
2023-10-26 13:10:42 +0300

    first commit

---
+++ santralistanbul-shuttleapp/forge.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  packagerConfig: {
+    asar: true,
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        certificateFile: './cert.pfx',
+        certificatePassword: process.env.CERTIFICATE_PASSWORD,
+        appDirectory: 'santralistanbul/out/my-app-64',
+        outputDirectory: 'santralistanbul/out/installer64',
+        authors: 'My App Inc.',
+        exe: 'myapp.exe'
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+  ],
+};
